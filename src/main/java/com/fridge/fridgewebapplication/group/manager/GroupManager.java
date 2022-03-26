40b1dfd6094638460258e5786bcc151d0d5c82b0
Tomasz <tomaszsiudak00@gmail.com>
2022-03-26 14:17:08 +0100

    GroupManager - wywołuje metody z GroupRepo

---
+++ src/main/java/com/fridge/fridgewebapplication/group/manager/GroupManager.java
@@ -0,0 +1,52 @@
+package com.fridge.fridgewebapplication.group.manager;
+
+import com.fridge.fridgewebapplication.group.dao.GroupRepo;
+import com.fridge.fridgewebapplication.group.dao.entity.Group;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class GroupManager {
+
+    private GroupRepo groupRepo;
+
+    @Autowired
+    public GroupManager(GroupRepo groupRepo) {
+        this.groupRepo = groupRepo;
+    }
+
+    public Iterable<Group> findAll() {
+        return groupRepo.findAll();
+    }
+
+    public Optional<Group> findById(int id) {
+        return groupRepo.findById(id);
+    }
+
+    public Group save(Group group) {
+        return groupRepo.save(group);
+    }
+
+    public void deleteById(int id) {
+        groupRepo.deleteById(id);
+    }
+
+    public Group findByName(String name) {
+        return groupRepo.findByName(name);
+    }
+
+    public Iterable<Group> findByArchived(boolean value) {
+        return groupRepo.findByArchived(value);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void fillDb() {
+        save(new Group("Nabial"));
+        save(new Group("Napoje"));
+        save(new Group("Mięso"));
+    }
+}
