286895d96bb94ed6bef29f53c1c234e63a85465b
Tomasz <tomaszsiudak00@gmail.com>
2022-03-26 13:31:32 +0100

    Initialize

---
+++ src/main/java/com/fridge/fridgewebapplication/FridgeWebApplication.java
@@ -0,0 +1,13 @@
+package com.fridge.fridgewebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgeWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fridge/fridgewebapplication/FridgeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fridge.fridgewebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
